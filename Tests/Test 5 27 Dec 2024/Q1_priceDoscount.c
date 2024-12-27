fc0bc0b3189b4cb8463925a8824a2480ce0d9dfd
Arjun Patel <hackmech007@gmail.com>
2024-12-27 18:47:48 +0530

    test 5 finished

--- Tests/Test 5 27 Dec 2024/Q1_priceDoscount.c
+++ Tests/Test 5 27 Dec 2024/Q1_priceDoscount.c
@@ -1,5 +1,6 @@
  #include<stdio.h>
 //question wrong needed profit term to calculate sp
+//assumption  - profit is zero
 
 float calSellPrice(float costPrice, int discount){
     return (costPrice*discount) / 100;
@@ -8,13 +9,14 @@ float calSellPrice(float costPrice, int discount){
  int main(){
     float costPrice;
     int discount;
-
+    int profit = 0;
 
     printf("Enter cost price of product.\n");
     scanf("%f", &costPrice);
     printf("Enter discount\n");
     scanf("%d", &discount);
 
-    printf("Selling price is %.2f", costPrice + calSellPrice(costPrice, discount));
+    printf("Selling price is %.2f\n", costPrice + (costPrice*profit)/100 - calSellPrice(costPrice, discount));
+    printf("Profit is %d%\n", profit);
     return 0;
  }
--- Tests/Test 5 27 Dec 2024/tempCodeRunnerFile.c
+++ Tests/Test 5 27 Dec 2024/tempCodeRunnerFile.c
@@ -1,6 +1,5 @@
 
-}
-
-
-int main(){
-    float amount;
+    printf("Selling price is %.2f\n", costPrice + (costPrice*profit)/100 - calSellPrice(costPrice, discount));
+    printf("Assumption - profit is negative");
+    return 0;
+ }
